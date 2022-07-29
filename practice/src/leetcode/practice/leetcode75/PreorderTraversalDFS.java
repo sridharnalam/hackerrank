00a38fae4efb9310ec48f6c42e4d0d17a417185d
nalams <sridhar.nalam@mavenir.com>
2022-07-29 16:42:21 +0900

    Java leetcode solution for N-ary Tree Preorder Traversal.

--- practice/src/leetcode/practice/leetcode75/PreorderTraversalDFS.java
+++ practice/src/leetcode/practice/leetcode75/PreorderTraversalDFS.java
@@ -1,20 +1,49 @@
 package leetcode.practice.leetcode75;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Stack;
 
+/**
+ * 589. N-ary Tree Preorder Traversal
+ */
 public class PreorderTraversalDFS {
     public static void main(String[] args) {
+        System.out.println(preorder(prepareInput()));
+    }
 
+    private static Node prepareInput() {
+
+        Node node1 = new Node(1);
+        Node node2 = new Node(2);
+        Node node3 = new Node(3);
+        Node node4 = new Node(4);
+        Node node5 = new Node(5);
+        Node node6 = new Node(6);
+
+        node3.children = Arrays.asList(node5, node6);
+        node1.children = Arrays.asList(node3, node2, node4);
+        return node1;
     }
 
     public static List<Integer> preorder(Node root) {
         List<Integer> list = new ArrayList<>();
-        Stack<Node> nodeStack= new Stack<>();
-        list.add(root.val);
+        Stack<Node> nodeStack = new Stack<>();
         nodeStack.push(root);
-        return null;
+        while (!nodeStack.empty()) {
+            Node parentNode = nodeStack.pop();
+            if (parentNode == null) {
+                break;
+            }
+            list.add(parentNode.val);
+            if (parentNode.children != null) {
+                for (int i = parentNode.children.size() - 1; i >= 0; i--) {
+                    nodeStack.push(parentNode.children.get(i));
+                }
+            }
+        }
+        return list;
     }
 
     static class Node {
