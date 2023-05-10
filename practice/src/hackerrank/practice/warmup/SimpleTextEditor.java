9abeecc00c225fec75cc723cfa40211edce54c6d
sridharnalam <sridharkumar777@gmail.com>
2023-05-10 21:34:01 +0900

    HackerRank java solution for Simple text editor

--- practice/src/hackerrank/practice/warmup/SimpleTextEditor.java
+++ practice/src/hackerrank/practice/warmup/SimpleTextEditor.java
@@ -3,43 +3,46 @@ package hackerrank.practice.warmup;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.Stack;
 
 // https://www.hackerrank.com/challenges/one-week-preparation-kit-simple-text-editor
 public class SimpleTextEditor {
+
     public static void main(String[] args) {
-        List<String> stateList = new ArrayList<>();
+        Stack<String> stack = new Stack<>();
         StringBuilder s = new StringBuilder();
-        stateList.add(s.toString());
+        stack.push(s.toString());
         Scanner scanner = new Scanner(System.in);
-        while (true) {
+        System.out.print("Enter num of operations : ");
+        int ops = Integer.parseInt(scanner.nextLine());
+
+        for(int i=0;i<ops;i++) {
             System.out.print("Enter op : ");
             String op = scanner.nextLine();
-            if(op.equalsIgnoreCase("bye")) {
-                break;
-            }
-            String[] ops = op.split(" ");
-            switch (Integer.parseInt(ops[0])) {
+            String[] cmd = op.split(" ");
+            switch (Integer.parseInt(cmd[0])) {
                 case 1:
-                    s.append(ops[1]);
-                    stateList.add(s.toString());
+                    s.append(cmd[1]);
+                    stack.push(s.toString());
                     break;
                 case 2:
-                    int range = Integer.parseInt(ops[1]);
+                    int range = Integer.parseInt(cmd[1]);
                     s.delete(s.length()-range, s.length());
-                    stateList.add(s.toString());
+                    stack.push(s.toString());
                     break;
                 case 3:
-                    System.out.println(s);
+                    int ind = Integer.parseInt(cmd[1]);
+                    System.out.println(s.charAt(ind-1));
                     break;
                 case 4:
-                    int index = stateList.size() - 2;
-                    if(index>=0) {
+                    stack.pop();
+                    if(!stack.isEmpty()) {
                         s.delete(0, s.length());
-                        s.append(stateList.get(index));
-                        stateList.remove(index+1);
+                        s.append(stack.peek());
                     }
                     break;
             }
         }
+        scanner.close();
     }
 }
